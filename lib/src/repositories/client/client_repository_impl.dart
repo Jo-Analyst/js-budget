a426630d3d704a10f4eea7668f205162d0e13f78
Joelmir R. Carvalho <joalves3carvalho@gmail.com>
2024-03-26 15:40:15 -0300

    removido o print

--- lib/src/repositories/client/client_repository_impl.dart
+++ lib/src/repositories/client/client_repository_impl.dart
@@ -87,7 +87,6 @@ class ClientRepositoryImpl implements ClientRepository {
   @override
   Future<Either<RespositoryException, Unit>> update(ClientModel client) async {
     try {
-      print(client.address!.toJson());
       final db = await DataBase.openDatabase();
       await db.transaction((txn) async {
         await txn.update(
