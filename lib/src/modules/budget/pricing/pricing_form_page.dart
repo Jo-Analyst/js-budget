7878b378eac05cec188c277790d11f53e5172ead
Joelmir Rogério Carvalho <joalves3carvalho@gmail.com>
2024-06-18 20:35:32 -0300

    retirado os imports

--- lib/src/modules/budget/pricing/pricing_form_page.dart
+++ lib/src/modules/budget/pricing/pricing_form_page.dart
@@ -1,7 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_getit/flutter_getit.dart';
 import 'package:js_budget/src/models/expense_model.dart';
 import 'package:js_budget/src/models/workshop_expense_items_budget_model.dart';
