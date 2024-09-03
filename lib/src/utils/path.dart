4f882d4e8c9e5e16940278112123a2b39f9992d0
Joelmir Rogério Carvalho <joalves3carvalho@gmail.com>
2024-09-03 09:56:04 -0300

    Finalizado a versão 1.0.0

--- lib/src/utils/path.dart
+++ lib/src/utils/path.dart
@@ -1,4 +1,5 @@
-const String directory = '/storage/emulated/0/.js_plan';
-const String pathStorageDatabase = '/storage/emulated/0/.js_plan/js_budget.db';
+const String directory = '/storage/emulated/0/js_planejar';
+const String pathStorageDatabase =
+    '/storage/emulated/0/js_planejar/js_budget.db';
 const String pathDatabase =
     '/data/user/0/com.example.js_budget/databases/js_budget.db';
